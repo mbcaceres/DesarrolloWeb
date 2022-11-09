b9af702a625485c646e50293d3e20f2f484455d7
Belen Caceres <belencaceres@live.com>
2022-11-09 16:02:01 -0300

    Entrega 2

--- js/main.js
+++ js/main.js
@@ -16,3 +16,43 @@ while (edad < 18) {
     edad = parseInt(prompt("Ingresá tu edad"));
 }
 console.log ('Listo!')
+
+
+function Logo( nombre, empleados, manual) 
+{
+    this.nombre = nombre;
+    this.empleados = empleados;
+    this.manual = manual; 
+}
+//cotizarLogo
+Logo.prototype.cotizarLogo = function () {
+    /*
+        1 = Ya está decidido 1.05
+        2 = Quiero crear nombre también 1.60
+    */ 
+
+    let cantidad;
+    const base = 2000;
+
+    switch (this.nombre) {
+        case '1':
+            cantidad = base * 1.05;
+            break;
+        case '2':
+            cantidad = base * 1.56;
+            break;} }
+
+    //Empleados
+
+    const empleados = {"De 1 a 30","De 30 a 100","Más de 100"};
+    console.log (empleados)
+
+
+// Manual de Marca
+
+   if (this.tipo === 'Sin manual') {
+       cantidad *= 1.30;
+   } else {
+       cantidad *= 1.50;
+   }
+    return cantidad; }
